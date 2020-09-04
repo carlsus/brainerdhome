cc6e86f9561fe70a6331ff091c3df1d0f59edfa1
Jed Malveda <jedmalveda@gmail.com>
2020-09-04 09:40:12 +0800

    Modified the request in controller

--- app/Http/Controllers/Buyer/BuyerRegistrationController.php
+++ app/Http/Controllers/Buyer/BuyerRegistrationController.php
@@ -17,16 +17,12 @@ class BuyerRegistrationController extends Controller
     public function create(BuyerRegistrationRequest $request)
     {
         // Sets the default user type on registration.
-        $user_type = 'buyer';
+        $request->request->add([
+            'user_type' => 'buyer'
+        ]);
         // Register the buyer.
         return User::create([
-            'firstname' => $request['firstname'],
-            'lastname' => $request['lastname'],
-            'mobilephone' => $request['mobilephone'],
-            'officephone' => $request['officephone'],
-            'email' => $request['email'],
-            'password' => Hash::make($request['password']),
-            'user_type' => $user_type,
+            $request->all()
         ]);
     } // create()
 }
