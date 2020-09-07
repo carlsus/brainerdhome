2934216d62e58eaf85986d18980bd19d6dd24fcc
Jed Malveda <jedmalveda@gmail.com>
2020-09-07 21:17:50 +0800

    Refactored the main view template app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,100 +16,13 @@
 	
     @yield('content')
 
-    <footer class="ftco-footer">
-      <div class="container">
-        <div class="row mb-5">
-			<div class="col-sm-12 col-md-6 col-lg-6">
-				
-    				<h2 class="mb-4" style="color: #ffff;">Join us Now!</h2>
-    				<form action="#" class="appointment">
-    					<div class="row">
-								<div class="col-md-6">
-									<div class="form-group">
-			              <input type="text" class="form-control" placeholder="First Name">
-			            </div>
-								</div>
-								<div class="col-md-6">
-									<div class="form-group">
-			              <input type="text" class="form-control" placeholder="Last Name">
-			            </div>
-								</div>
-								<div class="col-md-12">
-									<div class="form-group">
-										<input type="text" class="form-control" placeholder="Email">
-									  </div>
-								</div>
-								
-								<div class="col-md-12">
-									<div class="form-group">
-			              <input type="submit" value="Sign up" class="btn btn-primary py-3 px-4">
-			            </div>
-								</div>
-    					</div>
-	          		</form>
-			
-			  </div>
-          <div class="col-sm-12 col-md-3 col-lg-3">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2 logo"><a href="#">Visit us</a></h2>
-              
-              <ul class="ftco-footer-social list-unstyled mt-2">
-                <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
-              </ul>
-            </div>
-          </div>
-          
-          
-          <div class="col-sm-12 col-md-3 col-lg-3">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Sellerzz</h2>
-              <ul class="list-unstyled">
-                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Tools</a></li>
-                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Mortgage</a></li>
-                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Sell Your Home</a></li>
-                
-              </ul>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-      <div class="container-fluid px-0 py-5 bg-black">
-      	<div class="container">
-      		<div class="row">
-	          <div class="col-md-12">
-		
-	            <p class="mb-0" style="color: rgba(255,255,255,.5);"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> Sellerzz 
-	  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-	          </div>
-	        </div>
-      	</div>
-      </div>
-    </footer>
+    @include('layouts.parts.footer')
     
-  
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
-  <!-- loader -->
-  {{-- <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div> --}}
+    @include('layouts.scripts.common_scripts')
 
-
-  <script src="{{ asset('js/jquery.min.js') }}"></script>
-  <script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
-  <script src="{{ asset('js/popper.min.js') }}"></script>
-  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
-  <script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.stellar.min.js') }}"></script>
-  <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
-  <script src="{{ asset('js/jquery.animateNumber.min.js') }}"></script>
-  <script src="{{ asset('js/scrollax.min.js') }}"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="{{ asset('js/google-map.js') }}"></script>
-  <script src="{{ asset('js/main.js') }}"></script>
-  @yield('custom_scripts')
+    @yield('custom_scripts')
   </body>
 </html>
