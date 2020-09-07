0dd86c3661666703ebc5d0ab371028331b814f39
Jed Malveda <jedmalveda@gmail.com>
2020-09-07 19:19:34 +0800

    Commit after merge 09-07-2020

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-<<<<<<< HEAD
-Route::get('/buyers', 'BuyersController@index')->name('buyer_search');
-Route::get('/buyers/register', 'BuyersController@register')->name('buyer_register');
-=======
 // Buyer Routes
 Route::get('/buyer', 'Buyer\BuyerSearchController@index')->name('buyer.search.index');
 Route::get('/buyer/register', 'Buyer\BuyerRegistrationController@show')->name('buyer.register');
 Route::post('/buyer/register', 'Buyer\BuyerRegistrationController@create')->name('buyer.register');
 Route::get('/buyer/dashboard', 'Buyer\BuyerDashboardController@index')->middleware('auth')->name('buyer.dashboard');
->>>>>>> buyer_registration_feater_test
 
 Route::get('/seller', 'SellerController@index')->name('seller_index');
