601f614d8170123b7c4120983348b9a6bf66a571
Jed Malveda <jedmalveda@gmail.com>
2020-09-15 18:03:55 +0800

    Refactoring migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('user_type', ['buyer', 'seller']);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_09_15_095533_create_properties_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('type_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->decimal('price', 15, 2);
+            $table->string('hlurbno');
+            $table->string('listingno');
+            $table->string('address');
+            $table->date('listdate');
+            $table->string('lotarea');
+            $table->string('floorarea');
+            $table->string('bedrooms');
+            $table->string('bathrooms');
+            $table->string('garage');
+            $table->timestamps();
+            // Foreign Keys
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('type_id')->references('id')->on('propetry_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
